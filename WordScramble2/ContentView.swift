76d700c2ad4272b10241444ca01813be86f45549
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-11-22 06:04:47 +1030

    Initial Commit

---
+++ WordScramble2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble2
+//
+//  Created by Woodrow Martyr on 22/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble2/WordScramble2App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble2App.swift
+//  WordScramble2
+//
+//  Created by Woodrow Martyr on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
